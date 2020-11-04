0600c3674f8485cb0a075157522e0aeb6a624989
Laroshan Surendran <you@example.com>
2020-11-04 20:11:50 +0530

    med form

--- src/pages/EmpMedicalPage.js
+++ src/pages/EmpMedicalPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import MedicalForm from "../components/MedicalPage/MedicalForm";
-import Title from "../components/Title";
+//import Title from "../components/Title";
 
 export default function EmpMedicalPage() {
   return (
